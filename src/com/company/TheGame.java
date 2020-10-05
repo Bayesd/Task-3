d9f7efd1caad0b72d932ea2b4cfd0e59eb77e9ca
Leo <gbgleo@gmail.com>
2020-10-05 17:06:58 +0200

    Adding a branch with a new method

--- src/com/company/TheGame.java
+++ src/com/company/TheGame.java
@@ -1,8 +1,12 @@
 package com.company;
 import java.util.Scanner;
+import java.net.URI;
+
 
 public class TheGame {
     static boolean isRunning = true;
+
+
 
     public static void main(String[] args) {
     int i = 0;
@@ -16,17 +20,37 @@ public class TheGame {
                     isRunning = false;
                     System.out.println("You lost The Game.");
                 }  else if (i >= 1){
+                    secondLevel();
                     isRunning = false;
-                    System.out.println("You lost The Game.");
                 } else {
                     System.err.println("You are losing The Game.");
                     i++;
                 }
             } catch (Exception e) {
-                System.err.println("You really lost The Game.");
+                System.err.println("True or False?");
             }
 
         }
     }
+    public static void secondLevel(){
+        System.out.println("You are still losing The Game...");
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine();
+        try {
+            switch (in) {
+                case "what":
+                case "What?":
+                case "What game?":
+                case "What is The Game?":
+                    URI uri = new URI("https://sv.wikipedia.org/wiki/The_Game_(tankespel)");
+                    java.awt.Desktop.getDesktop().browse(uri);
+                    System.out.println("You lost The Game.");
+                    break;
+                default :
+                    System.out.println("You lost The Game.");
+            }
+        } catch (Exception e) {
+            System.err.println("You lost The Game.");
+        }
+    }
 }
-//          https://sv.wikipedia.org/wiki/The_Game_(tankespel)
