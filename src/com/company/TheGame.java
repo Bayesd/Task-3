6fbd3f405a0aabbc113308b298bdfd8673200eb1
Leo <gbgleo@gmail.com>
2020-10-05 16:34:54 +0200

    updated TheGame.java

--- src/com/company/TheGame.java
+++ src/com/company/TheGame.java
@@ -5,19 +5,25 @@ public class TheGame {
     static boolean isRunning = true;
 
     public static void main(String[] args) {
-
+    int i = 0;
         while (isRunning) {
             Scanner sc = new Scanner(System.in);
 
             try {
-                System.out.println("Welcome to The Game");
+                System.out.println("The Game is afoot.");
                 boolean in = sc.nextBoolean();
-                System.out.println("You lost The Game.");
                 if (in) {
                     isRunning = false;
+                    System.out.println("You lost The Game.");
+                }  else if (i >= 1){
+                    isRunning = false;
+                    System.out.println("You lost The Game.");
+                } else {
+                    System.err.println("You are losing The Game.");
+                    i++;
                 }
             } catch (Exception e) {
-                System.err.println("You lost The Game.");
+                System.err.println("You really lost The Game.");
             }
 
         }
